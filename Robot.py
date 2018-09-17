7a5d1ee588d2f8abee203ac51a1d084233ab886c
mbienvenu <42470122+mbienvenu@users.noreply.github.com>
2018-09-17 13:50:28 +1000

    class basic structure

--- Robot.py
+++ Robot.py
@@ -6,15 +6,38 @@ Created on Mon Sep 17 12:37:06 2018
 """
 
 class Robot():
-    pass
+    def __init__(self,x,y,dir):
+        self.x = x 
+        self.y = y
+        self.dir = dir 
+        
+    def place():
+        pass
+    
+    def move():
+        pass
+    
+    def left():
+        pass
+    
+    def right():
+        pass
+    
+    def report(self):
+        print(self.x,self.y,self.dir)
+    
+ 
 
 
-robot = Robot()
+robot = Robot(0,0,'NORTH')
 current.position = robot.report()
 robot.move()
 robot.left()
 robot.right()
 robot.place(0,0,'north')
+
+
+
 
 
 
