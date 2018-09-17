f82c0743d16c7b78a050aaa94067e646e81c0fc0
mbienvenu <42470122+mbienvenu@users.noreply.github.com>
2018-09-18 02:01:01 +1000

    added comments

--- toy-robot.py
+++ toy-robot.py
@@ -16,6 +16,8 @@ class Robot():
         pass
         
     def place(self,x,y,dir):
+# - place the robot on a specified sport using x , y as coordinates and 
+# dir as direction. If it's outside , send error message back to the console
         if x in range(0,5) and y in range(0,5):        
             self.x = x 
             self.y = y
@@ -24,6 +26,9 @@ class Robot():
             print("Robot is not placed in the right bounds of the table")
     
     def move(self):
+# - move the toy robot. Assess the current direction the robot is facing and
+# move the robot by 1 unit in that direction, after assessing if the move would
+# lead to the robot being out of bounds. Ranges are tested using TDD. 
         if self.dir == 'NORTH':
             if self.y in range(0,4):
                 self.y += 1
@@ -46,6 +51,8 @@ class Robot():
                 print("Robot will fall off the table on this move")
    
     def left(self):
+# Rotate the toy robot by 90 degrees counter-clockwise based on the current
+# direct it is facing
         if self.dir == 'NORTH':
             self.dir = 'WEST'
         elif self.dir == 'SOUTH':
@@ -56,6 +63,8 @@ class Robot():
             self.dir = 'NORTH'
     
     def right(self):
+# Rotate the toy robot by 90 degrees clockwise based on the current
+# direct it is facing
          if self.dir == 'NORTH':
             self.dir = 'EAST'
          elif self.dir == 'SOUTH':
@@ -66,17 +75,19 @@ class Robot():
             self.dir = 'SOUTH'
     
     def report(self):
+# Shout out the current coordinates and the direction the robot is facing.
+# Also returns a dictionary of the 3 values.
         print(self.x,',',self.y,',',self.dir)
         return {'x':self.x, 'y':self.y, 'dir': self.dir}
     
 
 
 def main():  
-   filepath = sys.argv[1]
+   filepath = sys.argv[1] # - Sys.argv[1] is populated from the command line
    if not os.path.isfile(filepath):
        print("File path {} does not exist. Exiting...".format(filepath))
        sys.exit()
-   robot = Robot()
+   robot = Robot() # create the instance of Robot and initialize
    with open(filepath) as fp:
        for line in enumerate(fp):
            if line[1].rstrip() == 'REPORT':
@@ -88,6 +99,7 @@ def main():
            elif line[1].rstrip() == 'LEFT':
                robot.left()
            elif line[1].rstrip().split(' ')[0] == 'PLACE':
+# - the read function in python returns tuples, hence the parsing method below
                placebot = line[1].rstrip().split(' ')[1]
                robot.place(int(placebot.split(',')[0]),int(placebot.split(',')[1]),
                            placebot.split(',')[2])
