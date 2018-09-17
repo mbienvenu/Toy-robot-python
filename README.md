bb9f1495cb2e622d1c598f2462f2d08f521e132b
mbienvenu <42470122+mbienvenu@users.noreply.github.com>
2018-09-18 01:39:35 +1000

    final code + README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Toy-robot-python
+
+##Please see toy-robot-py for final version of code
+##Test code is in Robot.py
+
+
+##To run the code, have python installed and
+## type > python toy-robot.py commands.txt < in the command prompt
+
+
+### Thank you ! ###
--- toy-robot.py
+++ toy-robot.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 17 12:37:06 2018
+Created on Tue Sep 18 01:22:38 2018
 
 @author: cecil
 """
+
 
 import numpy as np
 import pandas as pd
@@ -11,10 +12,8 @@ import sys
 import os
 
 class Robot():
-    def __init__(self,x,y,dir):
-        self.x = x 
-        self.y = y
-        self.dir = dir 
+    def __init__(self):
+        pass
         
     def place(self,x,y,dir):
         if x in range(0,5) and y in range(0,5):        
@@ -67,85 +66,18 @@ class Robot():
             self.dir = 'SOUTH'
     
     def report(self):
-        print(self.x,self.y,self.dir)
+        print(self.x,',',self.y,',',self.dir)
         return {'x':self.x, 'y':self.y, 'dir': self.dir}
- 
+    
 
 
-
-
-
-
-
-
-'''
-robot = Robot(0,0,'NORTH')
-current_position = robot.report()
-robot.move()
-current_position = robot.report()
-robot.left()
-current_position = robot.report()
-robot.move()
-current_position = robot.report()
-robot.right()
-current_position = robot.report()
-robot.move()
-current_position = robot.report()
-robot.place(-1,-1,'WEST')
-current_position = robot.report()
-robot.right()
-current_position = robot.report()'''
-
-
-
-
-'''
-commands = open("commands.txt","r")
-for line in commands.readlines():
-    if line == 'REPORT':
-        print('1')
-        current_position = robot.report()
-    elif line == 'MOVE':
-        print('2')
-        robot.move()
-    elif line == 'RIGHT':
-        print('3')
-        robot.right()
-    elif line == 'LEFT':
-        print('4')
-        robot.left()
-
-robot.report()'''
-'''
 def main():  
    filepath = sys.argv[1]
-
    if not os.path.isfile(filepath):
        print("File path {} does not exist. Exiting...".format(filepath))
        sys.exit()
-   robot = Robot(0,0,'NORTH')
-   current_position = robot.report()
+   robot = Robot()
    with open(filepath) as fp:
-       for line in enumerate(fp):
-           if line[1].rstrip() == 'REPORT':
-               current_position = robot.report()
-           elif line[1].rstrip() == 'MOVE':
-               robot.move()
-           elif line[1].rstrip() == 'RIGHT':
-               robot.right()
-           elif line[1].rstrip() == 'LEFT':
-               robot.left()
-           elif line[1].rstrip().find("PLACE"):
-               print(line)
-
-if __name__ == '__main__':  
-   main()
-'''
-
-'''
-robot = Robot(0,0,'NORTH')    
-filepath = 'commands.txt'
-with open(filepath) as fp:
        for line in enumerate(fp):
            if line[1].rstrip() == 'REPORT':
                current_position = robot.report()
@@ -160,5 +92,5 @@ with open(filepath) as fp:
                robot.place(int(placebot.split(',')[0]),int(placebot.split(',')[1]),
                            placebot.split(',')[2])
 
-current_position = robot.report()
-'''  
+if __name__ == '__main__':  
+   main()
