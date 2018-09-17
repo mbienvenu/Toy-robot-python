3d5c100e3e1a6d9a1cd13d25223ef1344f43fffc
mbienvenu <42470122+mbienvenu@users.noreply.github.com>
2018-09-17 13:23:10 +1000

    functions
    Initial functions

---
+++ Robot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 12:37:06 2018
+
+@author: cecil
+"""
+
+class Robot():
+    pass
+
+
+robot = Robot()
+current.position = robot.report()
+robot.move()
+robot.left()
+robot.right()
+robot.place(0,0,'north')
+
+
+
